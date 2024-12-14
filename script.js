dfe4e6d28c44272003b18c6f45b55f35fc1ce675
YoussefAbdelhameed <Youssef_Joe@gmail.com>
2024-12-14 17:14:43 +0200

    updating of style.css and script.js is correctly

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ let h1 = document.querySelector("h1")
 
 h1.innerHTML = "Youssef Joe"
 
+let div = document.createElement('div')
+div.style.width = "500px"
+div.style.height = "500px"
+div.style.backgroundColor = "#fff"
+div.style.borderRadius = "8px"
+div.style.padding = "15px"
+document.querySelector("body").appendChild(div)
+
 
 
